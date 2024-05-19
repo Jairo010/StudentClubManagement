afe6ced308e585de949ff8a8de2f6515f7e868f7
Jairo Freire <jairofreireortiz10@gmail.com>
2024-05-18 23:58:33 -0500

    The service has been implemented to manage everything from the members

--- frontend_app/src/app/services/api_serivices/members.service.ts
+++ frontend_app/src/app/services/api_serivices/members.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ISignUp } from '../../interfaces/userAuth.interface';
+import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { IMembers } from '../../members-list/members-list.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+
+  getMembers(): Observable<any>{
+    return this.http.get<any>(environment.URL_API+"members")
+  }
+
+  getMemberById(id:number): Observable<any>{
+    return this.http.get<any>(environment.URL_API+`members/${id}`)
+  }
+
+  updateMember(member:IMembers): Observable<IMembers>{
+    return this.http.put<IMembers>(environment.URL_API+`members`,member)
+  }
+
+  deleteMember(id:number): Observable<any>{
+    return this.http.delete<any>(environment.URL_API+`members/${id}`)
+  }
 }
