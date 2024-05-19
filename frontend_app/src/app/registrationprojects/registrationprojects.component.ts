376b943d2f6f3aa38f5d0b5c1b04ecd372ef00c0
Jairo Freire <jairofreireortiz10@gmail.com>
2024-05-18 23:53:20 -0500

    Methods for handling the resgistration projects form have been added.

--- frontend_app/src/app/registrationprojects/registrationprojects.component.ts
+++ frontend_app/src/app/registrationprojects/registrationprojects.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProjectsService } from '../services/api_serivices/projects.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
     selector: 'app-registrationprojects',
     standalone: true,
     templateUrl: './registrationprojects.component.html',
     styleUrl: './registrationprojects.component.css',
-    imports: []
+    imports: [FormsModule, ReactiveFormsModule]
 })
 export class RegistrationprojectsComponent {
-  members = [
-    { name: 'John Doe' },
-    { name: 'Jane Smith' },
-    { name: 'Alice Johnson'},
-    { name: 'Bob Davis' },
-  ];
+  member = inject(ProjectsService);
+
+  register = new FormGroup({
+    name: new FormControl<any>('', [Validators.required, Validators.email]),
+    description: new FormControl<any>('', ),
+    startDate: new FormControl<any>('', [Validators.required,]),
+    endDate: new FormControl<any>('', [Validators.required,]),
+    ckub: new FormControl<any>('', [Validators.required,]),
+  });
+
+
+  onSubmit(){
+    
+  }
 }
 
