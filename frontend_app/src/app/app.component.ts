2d39940b9bd64154f8dc2c43122fbfa42523d8b2
Joseph <josepcargua94@gmail.com>
2024-05-20 01:14:31 -0500

    Page replacement

--- frontend_app/src/app/app.component.ts
+++ frontend_app/src/app/app.component.ts
@@ -4,14 +4,14 @@ import { RegistrationprojectsComponent } from "./registrationprojects/registrati
 import { CoreModule } from "./core/core.module";
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { IonRouterOutlet } from "@ionic/angular/standalone";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RegistrationprojectsComponent, LoginComponent, CoreModule]
+    imports: [IonRouterOutlet, RouterOutlet, RegistrationprojectsComponent, LoginComponent, CoreModule]
 })
 export class AppComponent {
   title = 'frontend_app';
