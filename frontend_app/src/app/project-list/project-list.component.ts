f4b587bf840edbd8fae36cd7dc44d6acae179750
Jairo Freire <jairofreireortiz10@gmail.com>
2024-05-20 07:45:20 -0500

    Implemented the function to show all projects

--- frontend_app/src/app/project-list/project-list.component.ts
+++ frontend_app/src/app/project-list/project-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environment.development';
 import { MetaDataColumn } from '../shared/interfaces/metacolumn.interface';
+import { ProjectsService } from '../services/api_serivices/projects.service';
 
 export interface IProjects{
   name:string; 
@@ -18,38 +19,55 @@ export interface IProjects{
   styleUrl: './project-list.component.css'
 })
 export class ProjectListComponent {
-  data: IProjects[] = [
-    { name: 'Configuración de un software administrativo', description: 'Se realizara la configuración del sistema administrativo de la celula academica' },
-    { name: 'Recaudación de fondo para eventos', description: 'Se realizara diferentes actividades que ayuden a generar fondos para los diferentes eventos' },
-    { name: 'Venta de insumos tecnologicos', description:'Se realizara una venta de insumos tecnologicos para la obtención de fondos' },
-    { name: 'Programación de fiestas de la facultad', description: 'Organización de eventos para la comunidad universitaria' },
-    { name: 'Configuración de un software administrativo', description: 'Se realizara la configuración del sistema administrativo de la celula academica' },
-    { name: 'Recaudación de fondo para eventos', description: 'Se realizara diferentes actividades que ayuden a generar fondos para los diferentes eventos' },
-    { name: 'Venta de insumos tecnologicos', description:'Se realizara una venta de insumos tecnologicos para la obtención de fondos' },
-    { name: 'Programación de fiestas de la facultad', description: 'Organización de eventos para la comunidad universitaria' },
-    { name: 'Configuración de un software administrativo', description: 'Se realizara la configuración del sistema administrativo de la celula academica' },
-    { name: 'Recaudación de fondo para eventos', description: 'Se realizara diferentes actividades que ayuden a generar fondos para los diferentes eventos' },
-    { name: 'Venta de insumos tecnologicos', description:'Se realizara una venta de insumos tecnologicos para la obtención de fondos' },
-    { name: 'Programación de fiestas de la facultad', description: 'Organización de eventos para la comunidad universitaria' }
-  ];
+  private projectsService = inject(ProjectsService);
+
+  data:any = []
+
+  
   MetaDataColumn: MetaDataColumn[] = [
+    {field:'id', title:'Codigo'},
     {field:'name', title:'Nombre'},
-    {field:'description', title:'Descripción'},
+    {field:'description', title:'Descripcion'},
+    {field:'dateStart', title:'Fecha Inicio'},
+    {field:'dateEnd', title:'Fecha FIn'},
+    {field:'club', title:'Club'},
   ]
-  records: IProjects[]=[]
+  records:any =[]
   totalRecords = this.records.length
   constructor(){
     this.loadProjects()
   }
+  
+  field: any=[];
   loadProjects(){
-    this.records = this.data
-    this.totalRecords = this.records.length
-    this.changePage(0)
+    this.projectsService.getProjects().subscribe(
+      (data) =>{
+        
+        
+        this.records = data.data;
+        console.log(this.records);
+        this.records.forEach((dato:any) => {
+          
+          this.field.push({
+            id: dato.id,
+            name: dato.Nombre,
+            description: dato.Descripcion,
+            dateStart: dato.Fecha_Ini,
+            dateEnd: dato.Fecha_Fin,
+            club: dato.Id_Club.Nombre
+          });
+        })
+
+        this.totalRecords = this.records.length
+        this.changePage(0)
+      }
+    )
+    
   }
   changePage(page:number){
     const pageSize = environment.PAGE_SIZE
     const skip = pageSize * page
-    this.data = this.records.slice(skip, skip + pageSize)
+    this.data = this.field.slice(skip, skip + pageSize)
   }
   openForm(row:IProjects){
 
