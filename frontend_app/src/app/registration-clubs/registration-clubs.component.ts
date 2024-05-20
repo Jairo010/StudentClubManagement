344f7ffb08c9db0213ecb330c11358abc43e1ea3
Jairo Freire <jairofreireortiz10@gmail.com>
2024-05-20 07:46:39 -0500

    Implemented the function to register clubs

--- frontend_app/src/app/registration-clubs/registration-clubs.component.ts
+++ frontend_app/src/app/registration-clubs/registration-clubs.component.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { CoreModule } from '@angular/flex-layout';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ClubsService } from '../services/api_serivices/clubs.service';
+import { IClub } from '../interfaces/clubs.interface';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registration-clubs',
@@ -12,16 +15,35 @@ import { ClubsService } from '../services/api_serivices/clubs.service';
   styleUrl: './registration-clubs.component.css'
 })
 export class RegistrationClubsComponent {
-  member = inject(ClubsService);
-
+  clubs = inject(ClubsService);
+  router = inject(Router);
+  
   register = new FormGroup({
-    name: new FormControl<any>('', [Validators.required, Validators.email]),
+    name: new FormControl<any>('', [Validators.required,]),
     description: new FormControl<any>('', ),
     card: new FormControl<any>('', [Validators.required,]),
   });
 
 
   onSubmit(){
-    
+    if (this.register.valid) {
+      const clubData:IClub = {
+        name: this.register.get('name')?.value,
+        description: this.register.get('description')?.value,
+        cardResponsible: this.register.get('card')?.value,
+      };
+
+      this.clubs.createClub(clubData).subscribe(
+        response => {
+          console.log('Club registrado exitosamente', response);
+          this.router.navigate(['/clubs']);
+        },
+        error => {
+          console.error('Error al registrar el club', error);
+        }
+      );
+    } else {
+      console.log('Formulario inválido');
+    }
   }
 }
